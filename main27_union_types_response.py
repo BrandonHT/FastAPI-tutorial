19289762b3e5bc824bc988632769a7dd8e48da16
BrandonHdz2707 <brandonhdz2707@gmail.com>
2021-01-05 00:46:23 -0600

    Union types response and list response

--- main27_union_types_response.py
+++ main27_union_types_response.py
@@ -29,7 +29,33 @@ items = {
     },
 }
 
-
-@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem]) # type: ignore
 async def read_item(item_id: str):
     return items[item_id]
+'''
+### Returning a list of models ###
+
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str
+
+
+items = [
+    {"name": "Foo", "description": "There comes my hero"},
+    {"name": "Red", "description": "It's my aeroplane"},
+]
+
+
+@app.get("/items/", response_model=List[Item])
+async def read_items():
+    return items
+
+'''
